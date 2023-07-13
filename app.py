0f41219b6ce12fbc7282df456166e04dad514b19
Anton Zotov <zotoff@gmail.com>
2023-07-13 12:42:29 -0400

    Stable version w partial functionality (below) : -allows to choose a file to upload -validate file is a flat file (comma or tab) -uploads file for preview -shows a snapshot of the file -allows select a column for processing (+) added persistent storage to SQLite (+) reworked the web page to show list of files in db or 'No files' message (+) file is stored in SQLite database upon uploading (+) file list for the files in the db is shown and radio button     to select a file for processing (+) when a file is selected snapshot of it is shown in a table format below,     allowing to select a column for processing via a radio button == All above is finally working

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlite3
 import os
 import csv
 import pandas as pd
+from io import StringIO
 
 # Create Flask App instance
 app = Flask(__name__)
@@ -11,7 +12,7 @@ app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-app.config['DATABASE'] = 'file_storage.db'
+app.config['DATABASE'] = 'datasets.db'
 
 
 # Create database and table if they don't exist
@@ -19,12 +20,14 @@ def init_db():
     with app.app_context():
         db = get_db()
         cursor = db.cursor()
-        cursor.execute('''
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS files (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                filename TEXT NOT NULL UNIQUE
+                filename TEXT NOT NULL,
+                content TEXT NOT NULL,
+                delimiter TEXT NOT NULL
             )
-        ''')
+        """)
         db.commit()
 
 
@@ -59,38 +62,46 @@ def get_file_list():
     return cursor.fetchall()
 
 
-# Save uploaded file to database
-def save_file_to_db(filename):
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute('INSERT INTO files (filename) VALUES (?)', (filename,))
-    db.commit()
-
 
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
     init_db()
 
+    # Process selected file, if any
+    selected_file = request.form.get('selected_file')
+    data = None
+
+    if selected_file:
+        # Get the file content from the database
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT *  FROM files WHERE filename = ?", (selected_file,))
+        result = cursor.fetchone()
+        if result:
+            content = result[2]
+            dialect = csv.Sniffer().sniff(content, delimiters=result[3])
+            # Parse content into list of lists  
+            data = list(csv.reader(StringIO(content), dialect=dialect))[:10]
+        return render_template("upload.html", file_list=get_file_list(), selected_file=selected_file, data=data, error_message=None)
+
+
     if request.method == "POST":
         file = request.files["file"]
-        # Process the uploaded file here
-
-        # Analyze the content of the file to determine the separator
-        dialect = csv.Sniffer().sniff(file.read(1024).decode('utf-8'))
-        file.seek(0)
-
-        # Read the file into a pandas DataFrame
-        df = pd.read_csv(file, dialect=dialect)
-
-        # Save the file to the database
-        filename = secure_filename(file.filename)
-        save_file_to_db(filename)
-
-        # Get a snippet of the file data
-        data = df.head(20) if len(df) > 0 else None
-        columns = [data.columns.tolist()] + data.values.tolist()
-
-        return render_template("upload.html", file_list=get_file_list(), data=columns, error_message=None)
+        
+        # Validate the file format (comma or tab separated)
+        allowed_delimiters = [",", "\t"]
+        content = file.read().decode("utf-8")
+        dialect = csv.Sniffer().sniff(content, delimiters=allowed_delimiters)
+        if dialect.delimiter in allowed_delimiters:
+            # Save the file and its content to the database
+            db = get_db()
+            cursor = db.cursor()
+            cursor.execute("INSERT INTO files (filename, content, delimiter) VALUES (?, ?, ?)", (file.filename, content, dialect.delimiter))
+            db.commit()
+        else:
+            error_message = "Invalid file format. Only comma or tab-separated files are allowed."
+            return render_template("upload.html", file_list=get_file_list(), selected_file=None, error_message=error_message)
+        
 
     return render_template("upload.html", file_list=get_file_list(), data=None, error_message=None)
 
