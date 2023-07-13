64e365187968b3ac44306e5e828179235f7afd2b
Anton Zotov <zotoff@gmail.com>
2023-07-13 09:56:47 -0400

    Intermediate version for baselining (x) means broken (v) means fixed
    -allows to choose a file to upload
    -validate file is a flat file (comma or tab)
        (x) bug when not finding delimiter
    -uploads file for preview
    -shows a snapshot of the file
        (x) doesnt do anything when file selected in the list
    -allows select a column for processing
        (x) broken by the one above
    (+) added persistent storage to SQLite
    (+) reworked the web page to show list of files in db or 'No files' message
    (+) file is stored in SQLite database upon uploading
        (v) file content was not stored, only name, now fixed
    (+) file list for the files in the db is shown and radio button
        to select a file for processing
    (+) when a file is selected snapshot of it is shown in a table format below,
        allowing to select a column for processing via a radio button
        (v) bug: when radio button selected - nothing is shown

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, render_template, session, g
-from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template, g
 import sqlite3
-import os
 import csv
-import pandas as pd
+import os
 
 # Create Flask App instance
 app = Flask(__name__)
@@ -11,61 +9,37 @@ app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-app.config['DATABASE'] = 'file_storage.db'
+app.config['DATABASE'] = 'datasets.db'
 
+# Database initialization
 
-# Create database and table if they don't exist
-def init_db():
-    with app.app_context():
-        db = get_db()
-        cursor = db.cursor()
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS files (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                filename TEXT NOT NULL UNIQUE
-            )
-        ''')
-        db.commit()
-
-
-# Get database connection
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(app.config['DATABASE'])
     return db
 
-
-# Clear session and close database connection on shutdown
-@app.teardown_appcontext
-def close_db(error):
+def close_db(e=None):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
-
-# Clear session and remove uploaded file on startup
-@app.before_request
-def clear_session():
-    session.clear()
-    session['uploaded_file'] = None
-
-
-# Populate file list from database
-def get_file_list():
-    db = get_db()
+def init_db():
+    with app.app_context():
+        db = get_db()
     cursor = db.cursor()
-    cursor.execute('SELECT filename FROM files')
-    return cursor.fetchall()
 
-
-# Save uploaded file to database
-def save_file_to_db(filename):
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute('INSERT INTO files (filename) VALUES (?)', (filename,))
+    # Create the files table if it doesn't exist
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS files (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            content TEXT NOT NULL
+        )
+        """
+    )
     db.commit()
-
 
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
@@ -73,52 +47,45 @@ def upload_file():
 
     if request.method == "POST":
         file = request.files["file"]
-        # Process the uploaded file here
 
-        # Analyze the content of the file to determine the separator
-        dialect = csv.Sniffer().sniff(file.read(1024).decode('utf-8'))
-        file.seek(0)
+        # Validate the file format (comma or tab separated)
+        allowed_delimiters = [",", "\t"]
+        content = file.read().decode("utf-8")
+        dialect = csv.Sniffer().sniff(content, delimiters=allowed_delimiters)
+        if dialect.delimiter in allowed_delimiters:
+            # Save the file and its content to the database
+            db = get_db()
+            cursor = db.cursor()
+            cursor.execute("INSERT INTO files (name, content) VALUES (?, ?)", (file.filename, content))
+            db.commit()
+        else:
+            error_message = "Invalid file format. Only comma or tab-separated files are allowed."
+            return render_template("upload.html", file_list=get_file_list(), selected_file=None, error_message=error_message)
 
-        # Read the file into a pandas DataFrame
-        df = pd.read_csv(file, dialect=dialect)
+    selected_file_name = request.form.get("selected_file")
+    if selected_file_name:
+        selected_file = get_file(selected_file_name)
+    else:
+        selected_file = None
 
-        # Save the file to the database
-        filename = secure_filename(file.filename)
-        save_file_to_db(filename)
+    return render_template("upload.html", file_list=get_file_list(), selected_file=selected_file, error_message=None)
 
-        # Get a snippet of the file data
-        data = df.head(20) if len(df) > 0 else None
-        columns = [data.columns.tolist()] + data.values.tolist()
-
-        return render_template("upload.html", file_list=get_file_list(), data=columns, error_message=None)
-
-    return render_template("upload.html", file_list=get_file_list(), data=None, error_message=None)
-
-
-@app.route('/process', methods=['POST'])
-def process_file():
-    selected_file = request.form['selected_file']
-    selected_column = request.form['selected_column']
-
-    # Get the file data from the database
+def get_file_list():
     db = get_db()
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM files WHERE filename = ?', (selected_file,))
-    result = cursor.fetchone()
-    filename = result[1] if result else None
+    cursor.execute("SELECT name FROM files")
+    return cursor.fetchall()
 
-    if filename:
-        # Load the file data into a DataFrame
-        df = pd.read_csv(filename)
-
-        # Perform the desired processing on the selected column
-        data = df[selected_column]
-
-        # Perform the processing and return the result
-        # ... (Add your processing code here)
-        return f"Selected file: {filename}, Selected column: {selected_column}"
-
-    return "File not found"
+def get_file(file_name):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT content FROM files WHERE name = ?", (file_name,))
+    file_content = cursor.fetchone()
+    if file_content:
+        content = csv.reader(file_content[0].splitlines(), delimiter=',')
+        return list(content)
+    else:
+        return None
 
 
 if __name__ == '__main__':
